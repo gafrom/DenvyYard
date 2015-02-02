6fba2618051fe1ceab43fba4c1f7ed5b8d2275b2
Oleg Afanasyev <gafrom@gmail.com>
2015-02-02 17:41:31 +0300

    playgrounds and microposts editing

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -3,5 +3,19 @@ class PlaygroundsController < ApplicationController
 	  @playground = Playground.find(params[:id])
 	end
 
-	
+	def update
+		@playground = Playground.find(params[:id])
+	    if @playground.update_attributes(pg_params) 
+	      flash[:success] = "Successfuly updated" 
+	    else 
+	      flash[:error] = "Not updated"
+	    end
+	    redirect_to :back
+	end
+
+	private
+
+  def pg_params
+    params.require(:playground).permit(:name_RU, :text1, :text2, :text3, :text4, :price, :min_price)
+  end
 end
--- app/models/playground.rb
+++ app/models/playground.rb
@@ -1,4 +1,4 @@
 class Playground < ActiveRecord::Base
-	has_many :microposts
+ 	has_many :microposts
 	has_many :options
 end
