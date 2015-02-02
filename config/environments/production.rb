42b9372573024e56b53ff0244e99a1bccc89b7e8
Oleg Afanasyev <gafrom@gmail.com>
2015-02-02 09:10:53 +0300

    fixing encoding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'denvyyard.com',
+    user_name:            'denvyyard@gmail.com',
+    password:             'Q1w2E3r4',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
