74a20371a431bf2cc7839382b63d6b3c2740cdec
Oleg Afanasyev <gafrom@gmail.com>
2015-02-03 16:22:26 +0300

    fixed: arrows, home page, footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
 	def sending_email
 		@customer = params[:customer]
 		UserMailer.welcome_email(@customer).deliver
+  		flash[:success]="Заявка отправлена. Мы перезвоним Вам в ближайшее время."
   		redirect_to :back
   	end
 end
