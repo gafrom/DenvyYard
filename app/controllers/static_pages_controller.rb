b525dc4178f8fff1d65c31e4c7738e83a46c1030
Oleg Afanasyev <gafrom@gmail.com>
2015-02-03 17:44:30 +0300

    fixed: flash-success-notice

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
 	def home
+
 	end
 
 	def sending_email
 		@customer = params[:customer]
 		UserMailer.welcome_email(@customer).deliver
-  		flash[:success]="Заявка отправлена. Мы перезвоним Вам в ближайшее время."
+  		flash[:success] = "Заявка отправлена. Мы перезвоним Вам в ближайшее время."
   		redirect_to :back
   	end
 end
