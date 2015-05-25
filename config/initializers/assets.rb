1bed88a21ab3ee9510beca6c8734de1406917698
Oleg Afanasyev <gafrom@gmail.com>
2015-05-25 12:47:16 +0300

    adding welcoming pictures

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( jquery.inputmask.js )
+Rails.application.config.assets.precompile += %w( jquery.mobile.custom.min.js )
 Rails.application.config.assets.initialize_on_precompile = false
